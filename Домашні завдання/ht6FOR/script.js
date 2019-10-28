882f59c504ae756836ee9b3e9e7a0f7128ea1b91
Illia Vernygora <illiavernigora@gmail.com>
2019-10-28 18:06:26 +0200

    1-4

--- Домашні завдання/ht6FOR/script.js
+++ Домашні завдання/ht6FOR/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// // 1. Використовуючи цикли вивести на екран 20 символів «о».
+// 1. Використовуючи цикли вивести на екран 20 символів «о».
 
 for (let i = 0; i < 20; i++) {
     document.write(`o`)
@@ -10,23 +10,55 @@ for (let i = 0; i < 20; i++) {
 
 for (let i = 0; i < 7; i++) {
     for (let j = 0; j <= i; j++) {
-        document.write(`a`)
+        document.write(`o`)
     }
     document.write(`<br>`)
 }
 
 // 2.1 Відобразити трикутник за допомогою символів «о»
 
-let count0 = 1,
-    countProb = 6
+let countProb = 7
 for (let i = 0; i < 7; i++) {
     for (let j = 0; j < countProb; j++) {
-        document.write(`&nbsp;`)
+        document.write(`&nbsp;&nbsp;`)
     }
     countProb--
-    for (let j = 0; j < count0; j++) {
-        document.write(`0`)
+    for (let j = 0; j <= i; j++) {
+        document.write(`o`)
     }
-    count0++
     document.write("<br>")
+}
+
+//3. Відобразити трикутник за допомогою символів «о»
+
+for (let i = 0; i < 12; i++) {
+    for (let j = 12; j > i; j--) {
+        document.write(`o`)
+    }
+    document.write(`<br>`)
+}
+
+// 3.1 Відобразити трикутник за допомогою символів «о»
+
+let countProb = 12
+for (let i = 0; i < 12; i++) {
+    for (let j = 0; j <= i; j++) {
+        document.write(`&nbsp;&nbsp;`)
+    }
+    for (let j = 0; j < countProb; j++) {
+        document.write(`o`)
+    }
+    countProb--
+    document.write("<br>")
+}
+
+// 4. Побудувати ялинку
+
+for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j <= i; j++) {
+            document.write(`o`)
+        }
+        document.write(`<br>`)
+    }
 }
